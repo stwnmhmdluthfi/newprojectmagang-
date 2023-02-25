3a5bdc4b826d068c51e15dfce9242e3fc04feb15
mzufar-sabiq <zufarsabiq1@gmail.com>
2023-02-25 10:26:31 +0700

    perubahan

--- app/src/main/java/com/example/projectmagang/menu.kt
+++ app/src/main/java/com/example/projectmagang/menu.kt
@@ -1,69 +1,21 @@
 package com.example.projectmagang
 
-import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AlertDialog
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.azhar.absensi.R
-import com.azhar.absensi.utils.SessionLogin
-import com.azhar.absensi.view.absen.AbsenActivity
-import com.azhar.absensi.view.history.HistoryActivity
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var strTitle: String
-    lateinit var session: SessionLogin
+    private lateinit var Masuk: Button
+    private lateinit var Izin: Button
+    private lateinit var Histori: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+        Masuk = findViewById(R.id.btnMasuk)
+        Izin = findViewById(R.id.btnIzin)
+        Histori = findViewById(R.id.btnHistori)
 
-        setInitLayout()
-    }
-
-    private fun setInitLayout() {
-        session = SessionLogin(this)
-        session.checkLogin()
-
-        cvAbsenMasuk.setOnClickListener {
-            strTitle = "Absen Masuk"
-            val intent = Intent(this@MainActivity, AbsenActivity::class.java)
-            intent.putExtra(AbsenActivity.DATA_TITLE, strTitle)
-            startActivity(intent)
-        }
-
-        cvAbsenKeluar.setOnClickListener {
-            strTitle = "Absen Keluar"
-            val intent = Intent(this@MainActivity, AbsenActivity::class.java)
-            intent.putExtra(AbsenActivity.DATA_TITLE, strTitle)
-            startActivity(intent)
-        }
-
-        cvPerizinan.setOnClickListener {
-            strTitle = "Izin"
-            val intent = Intent(this@MainActivity, AbsenActivity::class.java)
-            intent.putExtra(AbsenActivity.DATA_TITLE, strTitle)
-            startActivity(intent)
-        }
-
-        cvHistory.setOnClickListener {
-            val intent = Intent(this@MainActivity, HistoryActivity::class.java)
-            startActivity(intent)
-        }
-
-        imageLogout.setOnClickListener {
-            val builder = AlertDialog.Builder(this@MainActivity)
-            builder.setMessage("Yakin Anda ingin Logout?")
-            builder.setCancelable(true)
-            builder.setNegativeButton("Batal") { dialog, which -> dialog.cancel() }
-            builder.setPositiveButton("Ya") { dialog, which ->
-                session.logoutUser()
-                finishAffinity()
-            }
-            val alertDialog = builder.create()
-            alertDialog.show()
-        }
     }
 
 }
